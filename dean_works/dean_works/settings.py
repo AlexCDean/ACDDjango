ed0ff891c387ad4e901e5e81490ff2b01797c507
Alex Dean <alex.dean@clyde.space>
2018-10-11 12:37:50 +0100

    landing page set up. need customization.

--- dean_works/dean_works/settings.py
+++ dean_works/dean_works/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'dean_works.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'personal_site', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- dean_works/dean_works/urls.py
+++ dean_works/dean_works/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from personal_site.urls import urlpatterns
+from personal_site.views import LandingPage
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', LandingPage.as_view(), name='landing_page'),
+    url(r'^admin/', admin.site.urls),
 ]
